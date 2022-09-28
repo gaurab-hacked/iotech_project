239af51cf87836fbc0aeaa463afe157a9f4dee2f
gaurab-hacked <gaurabsunar0001@gmail.com>
2022-09-28 23:07:34 +0545

    error and bugs fixed

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,8 @@
 import React, { useContext, useState } from 'react'
 import FormContext from '../context/FormContext'
 import LoginHTML from './LoginHTML';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Login = () => {
     const { postForm } = useContext(FormContext);
@@ -13,20 +15,26 @@ const Login = () => {
     //show value when click
     const submitBtnClk = (e) => {
         e.preventDefault();
-        if(formVal.username.length===0){
-            console.log("Please enter valid username")
+        if (formVal.username.length === 0) {
+            toast.error(<p id='error'>Username must be greater than 5 character</p>, {theme: "colored"});
         }
-        else if(formVal.password.length<6){
-            console.log("Please enter valid password")
+        else if (formVal.password.length < 6) {
+            toast.error(<p id='error'>Password must be greater than 5 character</p>, {theme: "colored"});
         }
-        else{
+        else {
             postForm(formVal)
-            setFormVal({username:"", password:""})
+            setFormVal({ username: "", password: "" })
+            toast.success(<p id='error'>Login Success</p>, {theme: "colored"});
         }
     }
 
     return (
-        <LoginHTML inpFldChange={inpFldChange} submitBtnClk={submitBtnClk} formVal={formVal} />
+        <>
+            <LoginHTML inpFldChange={inpFldChange} submitBtnClk={submitBtnClk} formVal={formVal} />
+            <ToastContainer
+                autoClose={2000}
+            />
+        </>
     )
 }
 
